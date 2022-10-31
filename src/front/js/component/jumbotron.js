506e3f10f0b967bd91cd3cd1b4ed067e51cdb61f
haukatkinson <haukatkinson@aol.com>
2022-10-31 01:33:28 +0000

    nav and jumbo started

---
+++ src/front/js/component/jumbotron.js
@@ -0,0 +1,14 @@
+const Jumbotron = () => {
+    return (
+      <div
+        className="p-5 text-center bg-image rounded-3"
+        style={{
+          backgroundImage: "url('jumboBanner.jpeg')",
+          backgroundSize: "cover",
+          height: "300px",
+          width: "100%",
+        }}
+      ></div>
+    );
+  };
+  export {Jumbotron}
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Brand
+        </a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
