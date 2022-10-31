89cd933b6b34f6521cf6bf123794edb90c6f7d92
haukatkinson <haukatkinson@aol.com>
2022-10-31 01:45:23 +0000

    backgroung img add to jumbo

--- src/front/js/component/jumbotron.js
+++ src/front/js/component/jumbotron.js
@@ -1,14 +1,17 @@
+import React, { Component } from "react";
+import banner from "./banner.jpg";
+
 const Jumbotron = () => {
-    return (
-      <div
-        className="p-5 text-center bg-image rounded-3"
-        style={{
-          backgroundImage: "url('jumboBanner.jpeg')",
-          backgroundSize: "cover",
-          height: "300px",
-          width: "100%",
-        }}
-      ></div>
-    );
-  };
-  export {Jumbotron}
+  return (
+    <div
+      className="p-5 text-center bg-image rounded-3"
+      style={{
+        backgroundImage: "url('banner.jpg')",
+        backgroundSize: "cover",
+        height: "800px",
+        width: "100%",
+      }}
+    ></div>
+  );
+};
+export { Jumbotron };
