9b4a160f08cb01772b18c3e164eb63fe9abc464d
haukatkinson <haukatkinson@aol.com>
2022-10-31 02:42:40 +0000

    logo added

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,55 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import navlogo from "./navlogo.png";
+import logo from "./navlogo2.png";
 
 export const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
-      <div class="container-fluid">
-        <img
-          src="navlogo.png"
-          alt="Girl in a jacket"
-          width="200"
-          height="100"
-        ></img>
-
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-mdb-toggle="collapse"
-          data-mdb-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i class="fas fa-bars"></i>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav>
+      <Link to={"/"}>
+        <img src={logo} className="logo w-25 p-3" />
+      </Link>
     </nav>
   );
 };
