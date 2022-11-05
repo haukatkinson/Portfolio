04d60863bdd86213f8d72ba852efd2a64ea22074
haukatkinson <haukatkinson@aol.com>
2022-11-05 16:04:01 +0000

    heroku deploy

--- src/app.py
+++ src/app.py
@@ -22,7 +22,9 @@ app.url_map.strict_slashes = False
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    if db_url and db_url.startswith("postgres://"):
+        db_url= db_url.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
